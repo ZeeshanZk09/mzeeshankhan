a6f5f02586b28479ce7dce0df54999b24accfa79
ZeeshanZk09 <mzeeshankhan0988@gmail.com>
2025-07-31 05:34:16 +0500

    Logout functionality mein behtari ki gayi hai. Token ko clear karne ke liye response object ko update kiya gaya hai aur successful logout ka message return kiya jata hai. Error handling ko bhi behtar banaya gaya hai.

--- src/app/api/auth/sign-out/route.ts
+++ src/app/api/auth/sign-out/route.ts
@@ -1,10 +1,12 @@
 import { NextResponse } from 'next/server';
 import userService from '@/services/userServices';
 
-export async function POST(response: NextResponse) {
+export async function POST() {
   try {
+    const response = NextResponse.json({ message: 'Logout successful' });
     const options = await userService.clearAuthCookie();
-    return response.cookies.set('token', '', options);
+    response.cookies.set('token', '', options); // remove the token
+    return response;
   } catch (error) {
     console.error('Logout error:', error);
     return NextResponse.json({ error: 'Failed to logout' }, { status: 500 });
